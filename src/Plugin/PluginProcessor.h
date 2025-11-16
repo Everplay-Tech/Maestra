1753bb22c6bb1c763b8e167ad8f3742a9f05b6c2
Everplay-Tech <info@shelleyguitar.com>
2025-11-16 15:34:57 -0600

    Create PluginProcessor.h

---
+++ src/Plugin/PluginProcessor.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <JuceHeader.h>
+#include "../Engine/OrchestraSynthEngine.h"
+#include "../Systems/PresetManager.h"
+#include "../Systems/Logger.h"
+#include "../Systems/PerformanceMonitor.h"
+
+class OrchestraSynthAudioProcessor  : public juce::AudioProcessor
+{
+public:
+    OrchestraSynthAudioProcessor();
+    ~OrchestraSynthAudioProcessor() override = default;
+
+    // AudioProcessor overrides
+    const juce::String getName() const override;
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    bool hasEditor() const override                                        { return true; }
+    juce::AudioProcessorEditor* createEditor() override;
+
+    double getTailLengthSeconds() const override                           { return 0.0; }
+
+    int getNumPrograms() override                                          { return 1; }
+    int getCurrentProgram() override                                       { return 0; }
+    void setCurrentProgram (int) override                                  {}
+    const juce::String getProgramName (int) override                       { return {}; }
+    void changeProgramName (int, const juce::String&) override            {}
+
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    OrchestraSynthEngine& getEngine() noexcept                             { return engine; }
+    PresetManager& getPresetManager() noexcept                             { return presetManager; }
+
+private:
+    Logger logger;
+    PerformanceMonitor perfMon { logger };
+    PresetManager presetManager;
+    OrchestraSynthEngine engine { presetManager, perfMon, logger };
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (OrchestraSynthAudioProcessor)
+};
