d6b80a793bab69725fe4ce48b668ed8d93fc4de8
Everplay-Tech <info@shelleyguitar.com>
2025-11-19 21:21:09 -0500

    Expose processor monitors and fix editor mixer wiring

--- src/Plugin/PluginEditor.cpp
+++ src/Plugin/PluginEditor.cpp
@@ -5,15 +5,9 @@ OrchestraSynthAudioProcessorEditor::OrchestraSynthAudioProcessorEditor (Orchestr
       processor (p),
       mixer (processor.getEngine(),
              processor.getPresetManager(),
-             processor.getEngine().perfMon, /* not accessible directly */
-             processor.getEngine().logger)  /* also not public */
+             processor.getPerformanceMonitor(),
+             processor.getLogger())
 {
-    // The above line won't compile as-is because perfMon/logger are private in the engine/processor.
-    // Instead, construct MixerComponent using the processor's public accessors we already have.
-
-    // Correct construction:
-    // (Adjust constructor arguments accordingly.)
-
 }
 
 void OrchestraSynthAudioProcessorEditor::paint (juce::Graphics& g)
